8243e06a66b662a94dae8fd29521778446e07d60
SSDD2022 <sil200201@hotmail.com>
2023-04-13 23:45:21 -0300

    Inicio

---
+++ taller/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context = { 'pagina' : 'taller\index.html',
+              }
+    return render(request,"taller\index.html",context)
