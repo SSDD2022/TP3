94cb3b30c96025d9304e92fba39209450dd00edb
SSDD2022 <sil200201@hotmail.com>
2023-04-22 16:30:58 -0300

    cursos sin estilo

---
+++ taller/classes.py
@@ -0,0 +1,6 @@
+class Curso ():
+    def __init__ (self,titulo,descripcion,tipo_participante,imagen):
+        self.titulo = titulo
+        self.descripcion = descripcion
+        self.tipo_participante = tipo_participante
+        self.imagen = imagen
--- taller/views.py
+++ taller/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from . import classes
 
 # Create your views here.
 def index(request):
@@ -11,5 +12,26 @@ def galeria(request):
     return render(request,"taller\galeria.html",context)
 
 def cursos(request):
-    context = {}
+    c1 = classes.Curso('Cerámica para niños',
+               'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+               'INFANTIL',
+               'taller\img\curso-ceramica_niños2.jpg')
+    c2 = classes.Curso('Cerámica para adultos',
+               'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+               'ADULTO',
+               'taller\img\curso-ceramica_adultos3.jpg')
+    c3 = classes.Curso('Alfarería',
+               'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+               'ADULTO',
+               'taller\img\curso-alfareria_adultos9.png')
+    c4 = classes.Curso('Esmaltado',
+               'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+               'ADULTO',
+               'taller\img\curso-esmaltado2.jpg')
+    c5 = classes.Curso('Especialización en arte precolombino',
+               'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+               'ADULTO',
+               'taller\img\curso-arte_precolombino.jpg')
+    ListadoCursos = [ c1, c2, c3, c4, c5 ]
+    context = { 'cursos': ListadoCursos}
     return render(request,"taller\cursos.html",context)
