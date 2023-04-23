bf1dba95cfea0f1326d3fe4baf3c6f0879dae11c
SSDD2022 <sil200201@hotmail.com>
2023-04-23 20:36:39 -0300

    cursos responsive

--- taller/views.py
+++ taller/views.py
@@ -12,7 +12,8 @@ def galeria(request):
     return render(request,"taller\galeria.html",context)
 
 def cursos(request):
-    c1 = classes.Curso('Cerámica para niños',
+    v1 = classes.Visual ("flush-headingOne","#flush-collapseOne","flush-collapseOne")
+    c1 = classes.Curso(1, 'Cerámica para niños',
                 ['El arte tiene importantes beneficios para los niños, jugando comparten y aprenden valores como el esfuerzo y la dedicación.',
                  'Mejora de la comunicación, pueden expresarse mediante sus obras.',
                  'Impulsa la creatividad e imaginación.',
@@ -20,8 +21,10 @@ def cursos(request):
                 ],
                'INFANTIL',
                'taller\img\curso-ceramica_niños2.jpg',
-               True)
-    c2 = classes.Curso('Cerámica para adultos',
+               True,
+               v1)
+    v2 = classes.Visual ("flush-headingTwo","#flush-collapseTwo","flush-collapseTwo")
+    c2 = classes.Curso(2, 'Cerámica para adultos',
                ['Cuentes con experiencia o estés comenzando, un mundo de posibilidades se despliega a partir de un trozo de archilla.',
                 'Objetos utilitarios como vasijas, platos, vasos, jarrones.',
                 'Figuras, muñecos, accesorios para bisutería, souvenirs',
@@ -30,8 +33,10 @@ def cursos(request):
                ],
                'ADULTO',
                'taller\img\curso-ceramica_adultos3.jpg',
-               True)
-    c3 = classes.Curso('Alfarería',
+               True,
+               v2)
+    v3 = classes.Visual ("flush-headingThree","#flush-collapseThree","flush-collapseThree")
+    c3 = classes.Curso(3, 'Alfarería',
                ['Las piezas realizadas en el torno comúnmente son concluidas manualmente, permitiendo todo tipo de intervención.',
                 'También esta técnica permite lograr una producción artesanal en serie',
                 'Taller dirigido a adultos.',
@@ -39,8 +44,10 @@ def cursos(request):
                ],
                'ADULTO',
                'taller\img\curso-alfareria_adultos9.png',
-               True)
-    c4 = classes.Curso('Esmaltado',
+               True,
+               v3)
+    v4 = classes.Visual ("flush-headingFour","#flush-collapseFour","flush-collapseFour")
+    c4 = classes.Curso(4, 'Esmaltado',
                ['El uso de esmaltes permite crear capas brillantes y mates, traslúcidas y opacas.',
                 'La manera de aplicar el producto varía según la forma de la pieza, su tamaño y también el acabado buscado.',
                 'Diferentes técnicas de esmaltado (por vertido, inmersión, pincel o pistola), permiten lograr resultados únicos.',
@@ -48,8 +55,10 @@ def cursos(request):
                ],
                'ADULTO',
                'taller\img\curso-esmaltado2.jpg',
-               True)
-    c5 = classes.Curso('Especialización en arte precolombino',
+               True,
+               v4)
+    v5 = classes.Visual ("flush-headingFive","#flush-collapseFive","flush-collapseFive")
+    c5 = classes.Curso(5, 'Especialización en arte precolombino',
                ['Investigamos el arte precolombino, desarrollado con materiales y herramientas disponibles en el entorno de las regiones en las que vivían las diferentes culturas.',
                 'Realizaban vasijas, utensilios y recipientes en general, instrumentos musicales, máscaras, juguetes, esculturas, entre otras.',
                 'Se destaca la representación de la naturaleza y la veneración a los dioses.',
@@ -58,7 +67,8 @@ def cursos(request):
                ],
                'ADULTO',
                'taller\img\curso-arte_precolombino.jpg',
-               False)
+               False,
+               v5)
     ListadoCursos = [ c1, c2, c3, c4, c5 ]
     context = { 'cursos': ListadoCursos}
     return render(request,"taller\cursos.html",context)
