1a8b46f76b0fa95c3eafbfd7a1cd513474678655
SSDD2022 <sil200201@hotmail.com>
2023-04-23 00:38:48 -0300

    cursos acordeón

--- taller/classes.py
+++ taller/classes.py
@@ -1,6 +1,7 @@
 class Curso ():
-    def __init__ (self,titulo,descripcion,tipo_participante,imagen):
+    def __init__ (self,titulo,descripcion,tipo_participante,imagen,cupo):
         self.titulo = titulo
         self.descripcion = descripcion
         self.tipo_participante = tipo_participante
         self.imagen = imagen
+        self.cupo = cupo
