e26676ee2753c3f458add473a31944e82b506066
ValAndFer <val.and.fer23@gmail.com>
2023-04-28 21:53:47 -0300

    se restablece tienda

--- tienda/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    context = { 'pagina' : 'tienda\index.html',
-              }
-    return render(request,"tienda\index.html",context)
