707679b931301a23f8be807f36330eba57fc71b6
SSDD2022 <sil200201@hotmail.com>
2023-05-03 22:56:19 -0300

    navegación de cursos a inscripción con parámetro

--- taller/urls.py
+++ taller/urls.py
@@ -1,12 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
 	path('',views.index,name="taller"),
-    path('galeria/',views.galeria,name="galeria"),
-    path('cursos/',views.cursos,name="cursos"),
-    path('alumnos/', views.alumnos,name="alumnos"),
-    path('cursos/inscripcion/<int:id_curso>/', views.inscripcion, name="inscripcion_curso"),
-    path('cursos/inscripcion/', views.inscripcion, name="inscripcion"),
-    path('agregar_trabajo/', views.agregar_trabajo, name="agregar_trabajo")
-    ]
+   path('galeria/',views.galeria,name="galeria"),
+   path('cursos/',views.cursos,name="cursos"),
+   path('alumnos/', views.alumnos,name="alumnos"),
+ #  re_path(r'^cursos/inscripcion/(?P<id_curso>[0-9]+)/$', views.inscripcion, name="inscripcion"),
+ #  re_path(r'^cursos/inscripcion/(?P<id_curso>\d{1,2})/$', views.inscripcion, name="inscripcion"),
+   path('cursos/inscripcion/<int:id_curso>/', views.inscripcion, name="inscripcion"),
+   path('cursos/inscripcion/', views.inscripcion, name="inscripcion"),
+   path('agregar_trabajo/', views.agregar_trabajo, name="agregar_trabajo")
+   ]
